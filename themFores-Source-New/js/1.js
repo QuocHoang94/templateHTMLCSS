1402669f3ed02d06c3932087405f4987529ba864
= <tqhoangcnttb94@gmail.com>
2020-07-08 16:46:13 +0700

    HTMLCSS

---
+++ themFores-Source-New/js/1.js
@@ -0,0 +1,14 @@
+$(function () {
+    khoimonan = $('.nhieumon').isotope({
+        // options
+        itemSelector: '.motmon',
+        layoutMode: 'masonry'
+    });
+
+    $('.tieudect a').click(function(){
+        dulieu = $(this).data('monan');
+        console.log(dulieu);
+        khoimonan.isotope({ filter: dulieu });
+        return false;
+    })
+})
